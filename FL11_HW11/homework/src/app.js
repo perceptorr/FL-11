e3acec6a1aee4e78bdb18e3b8d41a9d80cd245b9
Konstantin Varlamov <32339707+perceptorr@users.noreply.github.com>
2019-07-23 23:53:54 +0300

    Update app.js

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,41 +1,33 @@
 let rootNode = document.getElementById('root');
 rootNode.style.margin = '0 5%';
-
 let createdList = createList();
 
 function createList () {
   const header = document.createElement('h1');
   header.textContent = 'TODO Cat List';
-
   rootNode.appendChild(header);
 
   const notification = document.createElement('p');
   notification.innerHTML = 'Maximum item per list are created';
-
   rootNode.appendChild(notification);
 
   const adder = document.createElement('div');
-
   rootNode.appendChild(adder)
 
   const adderInput = document.createElement('input');
-  adderInput.setAttribute('placeholder', 'Add new action');
-  
+  adderInput.setAttribute('placeholder', 'Add new action');  
   adder.appendChild(adderInput);
 
   const adderImage = document.createElement('i');
   adderImage.classList.add('material-icons');
   adderImage.innerHTML = 'add_box';
-
   adder.appendChild(adderImage);
 
-  const line = document.createElement('hr');
-  
+  const line = document.createElement('hr'); 
   rootNode.appendChild(line)
 
   const list = document.createElement('div');
   list.classList.add('list');
-
   rootNode.appendChild(list);
 
   return {
@@ -51,7 +43,6 @@ function createEditImage() {
   editImage.classList.add('material-icons');
   editImage.classList.add('edit')
   editImage.innerHTML = 'create';
-
   return editImage
 }
 
@@ -60,7 +51,6 @@ function createDeleteImage() {
   deleteImage.classList.add('material-icons');
   deleteImage.classList.add('delete')
   deleteImage.innerHTML = 'delete';
-
   return deleteImage
 }
 
@@ -68,8 +58,7 @@ function createSaveImage() {
   let changeSaver = document.createElement('i')
   changeSaver.classList.add('material-icons');
   changeSaver.classList.add('save');
-  changeSaver.innerHTML = 'save';
-  
+  changeSaver.innerHTML = 'save';  
   return changeSaver
 }
 
